c9e9b7424bafc9bdad878da2424142dc1e9e3038
Romulo Rocha <romulorocha063@gmail.com>
2019-09-23 07:27:45 -0300

    styles in routes tabbar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,15 +3,16 @@ import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return <Background />;
 }
 
-Dashboard.navigationOptions = {
+Profile.navigationOptions = {
   tabBarLabel: 'Agendamentos',
   tabBarIcon: ({ tintColor }) => (
-    <Icon name="event" size={20} color={tintColor} />
+    <Icon name="person" size={20} color={tintColor} />
   ),
 };
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (isSigned = false) =>
   createAppContainer(
@@ -14,9 +15,22 @@ export default (isSigned = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: '#ffffff99',
+              style: {
+                backgroundColor: '#8d48a8',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'SignIn',
