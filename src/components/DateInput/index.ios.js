64333ae1ded0e60176b25f60c2f8956a0cdb5303
Romulo Rocha <romulorocha063@gmail.com>
2019-09-29 17:46:21 -0300

    add datepicker

---
+++ src/components/DateInput/index.ios.js
@@ -0,0 +1,36 @@
+import React, { useState, useMemo } from 'react';
+import { DatePickerIOS } from 'react-native';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons';
+import { Container, DateButton, DateText, Picker } from './styles';
+
+export default function DateInput({ date, onChange }) {
+  const [opened, setOpened] = useState(false);
+
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
+    [date]
+  );
+
+  return (
+    <Container>
+      <DateButton onPress={() => setOpened(!opened)}>
+        <Icon name="event" size={20} color="#fff" />
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+
+      {opened && (
+        <Picker>
+          <DatePickerIOS
+            date={date}
+            onDateChange={onChange}
+            minimumDate={new Date()}
+            minuteInterval={60}
+            locale="pt"
+          />
+        </Picker>
+      )}
+    </Container>
+  );
+}
